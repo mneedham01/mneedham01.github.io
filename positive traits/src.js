ce57ce4e96967d4a9d99ad4d5998fa7e983dfcff
mneedham01 <mneedham@smith.edu>
2025-02-20 21:41:32 -0500

    photos turn bedazzled when clicked

--- positive traits/src.js
+++ positive traits/src.js
@@ -1,9 +1,11 @@
 function enlarge(id) {
     elem = document.getElementById(id);
     elem.style.height = "100%";
+    elem.src = "../photos/" + id + "_bedazzled.png";
 }
 
 function shrink(id) {
     elem = document.getElementById(id);
     elem.style.height = "90%";
+    elem.src = "../photos/" + id + ".png";
 }
