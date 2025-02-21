a8eadb0176da70d25fcf0927f4e0ab610b3126d4
mneedham01 <mneedham@smith.edu>
2025-02-20 21:47:29 -0500

    bedazzled is based off of current webpage

--- positive traits/src.js
+++ positive traits/src.js
@@ -1,11 +1,17 @@
 function enlarge(id) {
+    // enlarge current & bedazzle 
     elem = document.getElementById(id);
     elem.style.height = "100%";
     elem.src = "../photos/" + id + "_bedazzled.png";
+
+    // shrink things to love 
+    shrink("things_to_love");
 }
 
 function shrink(id) {
     elem = document.getElementById(id);
     elem.style.height = "90%";
     elem.src = "../photos/" + id + ".png";
+    // enlarge things to love 
+    enlarge("things_to_love")
 }
