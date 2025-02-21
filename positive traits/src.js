8b50e60488263666dd9d10807d3bf40321a65140
mneedham01 <mneedham@smith.edu>
2025-02-20 22:03:11 -0500

    added index.html file

--- positive traits/src.js
+++ positive traits/src.js
@@ -1,3 +1,5 @@
+// this function takes a menu item, enlarges it & bedazzles it, 
+// while concurrently shrinking the things_to_love menu
 function enlarge(id) {
     // enlarge current & bedazzle 
     elem = document.getElementById(id);
@@ -8,9 +10,10 @@ function enlarge(id) {
     things_to_love = document.getElementById("things_to_love");
     things_to_love.style.height = "90%";
     things_to_love.src = "../photos/things_to_love.png";
-
 }
 
+// this function takes a menu item, shrinks it & de-bedazzles, 
+// while concurrently re-setting the things_to_love menu
 function shrink(id) {
     elem = document.getElementById(id);
     elem.style.height = "90%";
