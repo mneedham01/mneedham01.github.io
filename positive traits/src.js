bb0ac479b28cfa336e2c875b24a12c07775cdb97
mneedham01 <mneedham@smith.edu>
2025-02-20 21:14:16 -0500

    added images to menu, enlarged when you hover

---
+++ positive traits/src.js
@@ -0,0 +1,9 @@
+function enlarge(id) {
+    elem = document.getElementById(id);
+    elem.style.height = "120%";
+}
+
+function shrink(id) {
+    elem = document.getElementById(id);
+    elem.style.height = "90%";
+}
