02d28d1a7f78285bb225482044c31e4a87dceb32
mneedham01 <mneedham@smith.edu>
2025-02-28 16:01:39 -0500

    random elements scrolling across page

--- score/src.js
+++ score/src.js
@@ -1,20 +1,31 @@
-// figure changes randomly every 5 seconds -- needs to be some time consistency
-// maybe in another iteration, can change this by performer 
-// randomly cycles 
+// scroll across the screen in random order, connected back to back 
 const figure_ids = ["1", "2", "3", "4", "5", "6"];
 var prev_id;
+const element1 = document.getElementById("figure1");
+const element2 = document.getElementById("figure2");
+var prev_element = 1;
 
 setInterval(function figureAppears() {
+    // alternates between the two elements 
+    var element = prev_element == 1 ? element2 : element1;
     // then, choose a random new id 
     var min = 0; 
     var max = 5;
     var id = figure_ids[Math.floor(Math.random() * (max - min + 1)) + min];
     // change the source to the current id 
-    document.getElementById("figure").src = "../photos/score_pieces/figure" + id + ".png";
-    // make it visibile if it wasn't already 
-    if (prev_id == null) {
-        document.getElementById("figure").style.visibility = "visible";
-    }
+    element.src = "../photos/score_pieces/figure" + id + ".png";
+    // set to visible 
+    element.style.visibility = "visible";
     // set prev_id to current 
     prev_id = id;
+    // set prev_element 
+    prev_element = element.id == "figure1" ? 1 : 2;
+
+    // then, animate it 
+    // go from left = 100% to - 100%
+    element.animate (
+        {
+            left: ["100%", "-100%"]
+        }, 10000,
+    );
 }, 5000)
