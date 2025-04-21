a29011dc41d95d1f2846dd68f7473d1f97e23e57
mneedham01 <mneedham@smith.edu>
2025-04-21 19:12:49 -0400

    frame within frame

--- music_and_self/src.js
+++ music_and_self/src.js
@@ -1,16 +1,26 @@
 // this function will create a new iframe 
 // it will then put the code into this iframe 
-function nextSnippet(id) {
+function firstSnippet(id) {
     // find the path name 
     var path = id + ".html";
-    var doc = window.parent.document;
-    var iframe = doc.createElement("iframe");
+    var iframe = document.createElement("iframe");
     iframe.setAttribute("src", path);
     iframe.setAttribute("class", "frame");
     // append a line break 
-    linebreak = doc.createElement("br");
-    var body = doc.getElementById("body");
+    linebreak = document.createElement("br");
+    var body = document.getElementById("body");
     body.appendChild(linebreak);
     // append iframe 
     body.appendChild(iframe);
+}
+
+// this function will only put the code into the current iframe
+function nextSnippet(id) {
+    // find the path name 
+    var path = id + ".html";
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute("src", path);
+    iframe.setAttribute("class", "frame-within");
+    // append iframe 
+    document.body.appendChild(iframe);
 }
