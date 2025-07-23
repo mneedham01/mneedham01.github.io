ed005441c84d20b44b3da8ffd2b1f8b9c3fda407
mneedham01 <mneedham@smith.edu>
2025-07-23 02:15:44 -0400

    changed to no consecutive duplicates

--- concrete/src.js
+++ concrete/src.js
@@ -10,13 +10,25 @@
 
 // }, 5000);
 
+var lastPic = 1;
+
 function changePic() {
     console.log("in here");
     var background = document.getElementById("body");
-    var min = 1; 
-    var max = 13;
-    var pic_num = Math.floor(Math.random() * (max - min + 1)) + min;
+    var pic_num = randomNumber();
+    // check to make sure the current pic is not the same as the last
+    while (pic_num == lastPic) {
+        pic_num = randomNumber();
+    }
     var pic_name = "url(images/" + pic_num + ".jpg)";
 
     background.style.backgroundImage = pic_name;
+    lastPic = pic_num;
+}
+
+function randomNumber(){
+    var min = 1; 
+    var max = 13;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    
 }
